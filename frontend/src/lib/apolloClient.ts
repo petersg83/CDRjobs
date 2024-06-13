aaaa8804ff2efb654bf171dcf539add6ea1371e0
Pierre NOËL <petersg83@gmail.com>
2024-06-13 11:33:14 +0200

    use frontend env var

--- frontend/src/lib/apolloClient.ts
+++ frontend/src/lib/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: process.env.GRAPHQL_SERVER_URL,
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_SERVER_URL,
     cache: new InMemoryCache(),
   })
 }
